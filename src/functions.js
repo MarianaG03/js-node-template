440e71b3f0f50e27b6e1c440ed3d2691293f18bc
Mariana Garcia <mariana.garcia@student.swic.edu>
2025-02-27 22:02:54 -0600

    Test 2 Username format

--- src/functions.js
+++ src/functions.js
@@ -6,3 +6,13 @@ function calculateTotalPrice(order) {
 	return order.price * order.quantity;
 }
 console.log(calculateTotalPrice(order));
+
+const Username = {
+	firstname: "John",
+	lastname: "Doe",
+};
+
+function FormatUsername(Username) {
+	return Username.lastname + ", " + Username.firstname;
+}
+console.log(FormatUsername(Username));
