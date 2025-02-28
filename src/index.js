e1db93bda270971536a40bba5acecb679ac5da18
Mariana Garcia <mariana.garcia@student.swic.edu>
2025-02-27 21:54:06 -0600

    Add function examples

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-var x = 3;
+const student1 = {
+	name: "Sarah",
+	course: "JavaScript",
+	grade: 95,
+};
+const student2 = {
+	name: "John",
+	course: "Python",
+	grade: 88,
+};
+const student3 = {
+	course: "C++",
+	grade: 92,
+};
 
-console.log(x);
+function displayStudentInfo(student) {
+	return `${student.name} is taking ${student.course} and expects to earn ${student.grade}%`;
+}
+
+console.log(displayStudentInfo(student1));
+console.log(displayStudentInfo(student2));
+console.log(displayStudentInfo(student3));
+
+function add2Numbers(x, y) {
+	return x + y;
+}
+const sum = add2Numbers(3, 5);
+console.log(sum);
