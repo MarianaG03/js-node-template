30d4de21f7172d93739343db565ff56b2650e5ee
Mariana Garcia <mariana.garcia@student.swic.edu>
2025-02-27 22:05:30 -0600

    Test 3 Hello World

--- src/functions.js
+++ src/functions.js
@@ -16,3 +16,13 @@ function FormatUsername(Username) {
 	return Username.lastname + ", " + Username.firstname;
 }
 console.log(FormatUsername(Username));
+
+const world = {
+	string1: "Hello",
+	string2: "World",
+};
+
+function CombineStrings(world) {
+	return world.string1 + " " + world.string2;
+}
+console.log(CombineStrings(world));
