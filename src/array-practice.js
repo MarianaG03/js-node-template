2822c9e5efe05f390aa930c5fd264602485606b3
Mariana Garcia <mariana.garcia@student.swic.edu>
2025-03-09 21:26:25 -0500

    create Reduce method to calculate total cost of Total BackPack.

--- src/array-practice.js
+++ src/array-practice.js
@@ -58,3 +58,18 @@ console.log(playersWithMoreFraqsThanDeaths);
 const sortedPlayersbyFrags = players.sort((a, b) => b.frags - a.frags);
 console.log(sortedPlayersbyFrags);
 //Will sort the players by frags in descending order.
+
+// Coding Challenge!!
+
+//Coding Challenge 1
+const backpackFullOfAmmo = [
+	{ item: "bullets", quantity: 10, priceEa: 0.5 },
+	{ item: "shotgun shells", quantity: 4, priceEa: 0.5 },
+	{ item: "rockets", quantity: 1, priceEa: 5.0 },
+	{ item: "energy cell units", quantity: 20, priceEa: 0.2 },
+];
+const totalCost = backpackFullOfAmmo.reduce(
+	(total, item) => total + item.quantity * item.priceEa,
+	0,
+);
+console.log(totalCost);
