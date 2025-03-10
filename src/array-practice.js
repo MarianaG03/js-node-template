5c1ae0e85b75fc8e041177bcd201e2cc978cb370
Mariana Garcia <mariana.garcia@student.swic.edu>
2025-03-09 20:24:21 -0500

    Using > to determine players that have more frags then deaths and displaying the ture boolean.

--- src/array-practice.js
+++ src/array-practice.js
@@ -10,3 +10,17 @@ console.log(numbers);
 //Putting Filters and Modulus Together
 const oddNumbers = numbers.filter((number) => number % 2);
 console.log(oddNumbers);
+//Exercise
+const players = [
+	{ name: "DoomGuy", frags: 25, deaths: 0 },
+	{ name: "Slayer", frags: 25, deaths: 1 },
+	{ name: "Marine", frags: 12, deaths: 15 },
+	{ name: "Noob", frags: 0, deaths: 25 },
+];
+
+const playersWithMoreFragsThanDeaths = players.filter(
+	(player) => player.frags > player.deaths,
+);
+//Console will log the players with more frags than deaths useing the greater than operator to show more frage than deaths.
+// If More frags boolean is true, the player will be logged.
+console.log(playersWithMoreFragsThanDeaths);
