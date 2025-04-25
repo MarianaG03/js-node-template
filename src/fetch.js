2442d631ed264ec25c95cf58d2652f20e242bb93
Mariana Garcia <mariana.garcia@student.swic.edu>
2025-04-24 19:53:05 -0500

    reformatted promises to easier to read

--- src/fetch.js
+++ src/fetch.js
@@ -1,5 +1,7 @@
-fetch('https://www.swapi.tech/api/people/2').then(response =>
-{
-
-response.json()).then{(data) =>
-    console.log(data); }
+fetch('https://www.swapi.tech/api/people/2')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data) 
+})
